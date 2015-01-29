66a69d65ae33db9aa28b738f8b6c0c6e95081465
actiontwo <actiontwo@designveloper.com>
2015-01-29 08:09:22 +0700

    add lib jquery, and fonts style

---
+++ html/js/script.js
@@ -0,0 +1,24 @@
+// =============================================================================
+// ,,,,,,,,, ,,,
+// ,,,,,,,, ,,,  Copyright:
+// ,,,     ,,,          This source is subject to the Designveloper JSC
+// ,,,    ,,,           All using or modify must have permission from us.
+// ,,,   ,,,            http://designveloper.com
+// ,,,,,,,,
+// ,,,,,,,       Name:  DSVScriptTemplate
+//
+// Purpose:
+//          Describe the purpose of the script [short version]
+// Class:
+//          one ; two ; three
+// Functions:
+//          one ; two ; three
+// Called From:
+//          (script) any
+// Author:
+//          actiontwo
+// Notes:
+//          Additional information [long version]
+// Changelog:
+//          1/29/15 - actiontwo - Init first revision.
+// =============================================================================
